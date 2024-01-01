df892d7a8cf67d26172ce0ff892e69dd0121c3c8
Evyatar <code@ealush.com>
2024-01-02 00:58:54 +0200

    Remove nx

--- packages/flairup/src/utils/forIn.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.forIn = void 0;
-function forIn(obj, fn) {
-    for (var key in obj) {
-        fn(key.trim(), obj[key]);
-    }
-}
-exports.forIn = forIn;
