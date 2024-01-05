409b5ea8c9027040b5078c0e3d34b841ec1e191e
Evyatar <code@ealush.com>
2024-01-05 14:30:51 +0200

    Refine scope styles

--- src/types.ts
+++ src/types.ts
@@ -12,7 +12,10 @@ export type Styles = Partial<StyleObject & FlairUpProperties & Chunks>;
 export type StoredStyles = Record<string, [property: string, value: string]>;
 
 // This is the actual type that's returned from each create function
-export type ScopedStyles<K extends string = string> = Record<S<K>, ClassSet>;
+export type ScopedStyles<K extends string = string> = Record<
+  S<K & string>,
+  ClassSet
+>;
 export type ClassList = (string | undefined)[];
 export {};
 
