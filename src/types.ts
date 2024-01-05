8f51e05ebadff9fb0e4681665d93acaccf3b64c8
Evyatar <code@ealush.com>
2024-01-05 03:41:51 +0200

    Improved ScopedStyles generic

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-export type PropertyValue = string | number;
 export type StyleObject = Partial<CSSStyleDeclaration>;
 type Pseudo = `:${string}`;
 type MediaQuery = `@media ${string}`;
@@ -13,7 +12,7 @@ export type Styles = Partial<StyleObject & FlairUpProperties & Chunks>;
 export type StoredStyles = Record<string, [property: string, value: string]>;
 
 // This is the actual type that's returned from each create function
-export type ScopedStyles<K extends string> = Record<K | string, ClassSet>;
+export type ScopedStyles<K extends string> = Record<K, ClassSet>;
 export type ClassList = (string | undefined)[];
 export {};
 
@@ -24,5 +23,5 @@ type FlairUpProperties = Partial<{
 type Chunks = Record<Pseudo | MediaQuery, StyleObject>;
 
 export type CreateSheetInput<K extends string> =
-  | Record<K | string, Styles>
-  | Record<ParentClass, Record<K | string, Styles>>;
+  | Record<K, Styles>
+  | Record<ParentClass, Record<K, Styles>>;
